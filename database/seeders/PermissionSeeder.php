0402aaaa2b69006ebcf99babd370af65bb2880bd
ChenYu Xiao <apple.iiiyu@gmail.com>
2020-10-25 23:40:29 +1300

    update seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Dcat\Admin\Models\Permission;
+use Illuminate\Support\Facades\DB;
 
 class PermissionSeeder extends Seeder
 {
@@ -15,52 +16,52 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         //
-        if (Permission::where('slug', 'premium')->count() > 0) {
-            return 0;
+        if (Permission::where('id', 7)->count() == 0) {
+            DB::transaction(function () {
+                $createdAt = date('Y-m-d H:i:s');
+                Permission::insert([
+                    [
+                        'id'          => 7,
+                        'name'        => 'Premium',
+                        'slug'        => 'premium',
+                        'http_method' => '',
+                        'http_path'   => '',
+                        'parent_id'   => 0,
+                        'order'       => 7,
+                        'created_at' => $createdAt,
+                    ],
+                    [
+                        'id'          => 8,
+                        'name'        => 'Companies',
+                        'slug'        => 'companies',
+                        'http_method' => '',
+                        'http_path'   => '/companies*',
+                        'parent_id'   => 7,
+                        'order'       => 8,
+                        'created_at' => $createdAt,
+                    ],
+                    [
+                        'id'          => 9,
+                        'name'        => 'Series',
+                        'slug'        => 'series',
+                        'http_method' => 'GET',
+                        'http_path'   => '/series*',
+                        'parent_id'   => 7,
+                        'order'       => 9,
+                        'created_at' => $createdAt,
+                    ],
+                    [
+                        'id'          => 10,
+                        'name'        => 'Active Stock',
+                        'slug'        => 'active-stock',
+                        'http_method' => 'GET',
+                        'http_path'   => '/active-stocks*',
+                        'parent_id'   => 0,
+                        'order'       => 10,
+                        'created_at' => $createdAt,
+                    ],
+                ]);
+            });
         }
-
-        $createdAt = date('Y-m-d H:i:s');
-        Permission::insert([
-            [
-                'id'          => 7,
-                'name'        => 'Premium',
-                'slug'        => 'premium',
-                'http_method' => '',
-                'http_path'   => '',
-                'parent_id'   => 0,
-                'order'       => 7,
-                'created_at' => $createdAt,
-            ],
-            [
-                'id'          => 8,
-                'name'        => 'Companies',
-                'slug'        => 'companies',
-                'http_method' => '',
-                'http_path'   => '/companies*',
-                'parent_id'   => 7,
-                'order'       => 8,
-                'created_at' => $createdAt,
-            ],
-            [
-                'id'          => 9,
-                'name'        => 'Series',
-                'slug'        => 'series',
-                'http_method' => 'GET',
-                'http_path'   => '/series*',
-                'parent_id'   => 7,
-                'order'       => 9,
-                'created_at' => $createdAt,
-            ],
-            [
-                'id'          => 10,
-                'name'        => 'Active Stock',
-                'slug'        => 'active-stock',
-                'http_method' => 'GET',
-                'http_path'   => '/active-stocks*',
-                'parent_id'   => 0,
-                'order'       => 10,
-                'created_at' => $createdAt,
-            ],
-        ]);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Dcat\Admin\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -16,12 +17,21 @@ class RoleSeeder extends Seeder
     {
         //
 
-        Role::upsert([[
-            'name'       => 'Member',
-            'slug'       => 'member',
-        ], [
-            'name'       => 'Senior Member',
-            'slug'       => 'senior_member',
-        ]], ['slug'], ['name']);
+        if (Role::where('id', 2)->count() == 0) {
+            DB::transaction(function () {
+                Role::insert([
+                    [
+                        'id'         => '2',
+                        'name'       => 'Member',
+                        'slug'       => 'member',
+                    ],
+                    [
+                        'id'         => '3',
+                        'name'       => 'Senior Member',
+                        'slug'       => 'senior_member',
+                    ]
+                ]);
+            });
+        }
     }
 }
