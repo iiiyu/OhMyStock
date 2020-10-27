1a24353455b24f9dfd325b3e2318201c75f9fdc2
ChenYu Xiao <apple.iiiyu@gmail.com>
2020-10-27 20:59:08 +1300

    add schedule take

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('stock:iex:historical:all')->weekdays()->at('16:05');
     }
 
     /**
@@ -34,8 +34,13 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    protected function scheduleTimezone()
+    {
+        return 'America/New_York';
     }
 }
