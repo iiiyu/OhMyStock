73c725b0d2d2b344e6b13cd3b28a92219f3963ae
Xiao ChenYu <apple.iiiyu@gmail.com>
2021-03-07 01:46:50 +1300

    add yf historical data

--- data_src/import_yf_data/import_yf_data.py
+++ data_src/import_yf_data/import_yf_data.py
@@ -2,9 +2,9 @@ import requests
 from import_yf_data import import_yf_data_api
 from flask import current_app
 from flask_pymongo import PyMongo
-import datetime
+from datetime import datetime
 import yfinance as yf
-
+import re
 
 @import_yf_data_api.route("/test")
 def test():
@@ -19,70 +19,73 @@ def test():
 
 @import_yf_data_api.route("/data/yf/companies")
 def update_yf_companies():
-
     # search nsdaq company data 
     mongo = PyMongo(current_app)
     nasdaq_companies_collection = mongo.db.nasdaq_companies
     result = nasdaq_companies_collection.find({}, {'symbol': 1})
-    yf_companies_collection = mongo.db.yf_companies
-
+    i = 0
     for item in result:
+        i = i + 1
         if(item['symbol'].find("^") == -1):
             symbol = item['symbol'].replace("/", "-")
-            try:
-                company = yf.Ticker(symbol)
-                company_info = company.info
-                yf_companies_collection.update({'symbol':company_info['symbol']}, company_info, upsert=True)
-            except Exception as e:
-                pass
-            continue
-
+            update_yf_company(symbol)
+        
+        if (i == 5):
+            break
 
     return {
         'Code': 0,
         'Message': "update-companies"
     }
 
-
-# Install the Python Requests library:
-# `pip install requests`
-
-@import_yf_data_api.route("/test-request")
-def send_request():
-    # Get nadaq all companies list
-    # GET https://api.nasdaq.com/api/screener/stocks
-
-    # try:
-    #     response = requests.get(
-    #         url="https://api.nasdaq.com/api/screener/stocks",
-    #         params={
-    #             "tableonly": "true",
-    #             "limit": "25",
-    #             "offset": "0",
-    #             "download": "true",
-    #         },
-    #         headers={
-    #             "Authority": "api.nasdaq.com",
-    #             "Accept": "application/json, text/plain, */*",
-    #             "Origin": "https://www.nasdaq.com",
-    #             "Sec-Fetch-Site": "same-site",
-    #             "Sec-Fetch-Mode": "cors",
-    #             "Sec-Fetch-Dest": "empty",
-    #             "Referer": "https://www.nasdaq.com/",
-    #             "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-    #             "Accept-Encoding": "gzip",
-    #         },
-    #     )
-    #     print('Response HTTP Status Code: {status_code}'.format(
-    #         status_code=response.status_code))
-    #     print('Response HTTP Response Body: {content}'.format(
-    #         content=response.content))
-    # except requests.exceptions.RequestException:
-    #     print('HTTP Request failed')
+@import_yf_data_api.route("/data/yf/company/<symbol>")
+def update_yf_company(symbol):
+    now = datetime.now()
+    try:
+        mongo = PyMongo(current_app)
+        yf_companies_collection = mongo.db.yf_companies
+        company = yf.Ticker(symbol)
+        company_info = company.info
+        company_info['updated_at'] = now
+        yf_companies_collection.update({'symbol':company_info['symbol']}, company_info, upsert=True)
+    except Exception as e:
+        return {
+            'Code': 1001,
+            'Message': 'YF Fetch Data faile',
+            'Error' :  str(e)
+        }
 
     return {
         'Code': 0,
-        'Message': "update-companies"
+        'Message': "update yf company " + symbol
+    }
+
+
+
+@import_yf_data_api.route("/data/yf/historical/<symbol>")
+def update_yf_historical(symbol):
+    now = datetime.now()
+    try:
+        mongo = PyMongo(current_app)
+        collection_name = symbol + '_historical_collection'
+        historical_collection = mongo.db[collection_name]
+        company = yf.Ticker(symbol)
+        historical = company.history(period="max",prepost=True)
+        data = historical.to_dict('index')
+        for key in data:
+            new_item = data[key]
+            new_item['date'] = datetime.combine(key.date(), datetime.min.time())
+            new_item['updated_at'] = now
+            historical_collection.update({'date':new_item['date']}, new_item, upsert=True)
+    except Exception as e:
+        return {
+            'Code': 1001,
+            'Message': 'YF Fetch Data faile',
+            'Error' :  str(e)
+        }
+    return {
+        'Code': 0,
+        'Message': "update yf historical " + symbol
     }
 
 
